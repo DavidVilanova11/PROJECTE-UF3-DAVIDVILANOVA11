3467f3a0ab8e793444d87add56da01282386fe89
daw25 <navjashan.singh@cirvianum.cat>
2024-02-15 17:24:56 +0100

    rename 4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once("App/config.php");
 include_once("App/Router.php");
 include_once("App/Models/User.php");
 include_once("App/Models/Usuari.php");
-include_once("App/Models/Extinguides.php");
+include_once("App/Models/Extinguida.php");
 include_once("App/Core/Controller.php");
 require_once(__DIR__ . '/vendor/autoload.php');
 
