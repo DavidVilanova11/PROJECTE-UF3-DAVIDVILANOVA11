d2d57bc76e856fcd3c475e8d00bfe6b2ce02c5ca
daw25 <navjashan.singh@cirvianum.cat>
2024-02-16 16:10:11 +0100

    autoload

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 
-if (!isset($_SESSION)) {
-    session_start();
-}
-include_once("App/config.php");
-include_once("App/Router.php");
-include_once("App/Models/User.php");
-include_once("App/Models/Usuari.php");
-include_once("App/Models/Recuperada.php");
-include_once("App/Core/Controller.php");
-require_once(__DIR__ . '/vendor/autoload.php');
+require_once(__DIR__ . "/App/autoload");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $r = new Router();
 $r->run();
