e840a2a33a4123e9f529ab5b80e34179e1986cc3
daw25 <navjashan.singh@cirvianum.cat>
2024-02-22 17:14:13 +0100

    commit autoload

--- App/autoload.php
+++ App/autoload.php
@@ -1,11 +1,3 @@
 <?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-include_once("App/config.php");
-include_once("App/Router.php");
-include_once("App/Models/User.php");
-include_once("App/Models/Usuari.php");
-include_once("App/Models/Recuperada.php");
-include_once("App/Core/Controller.php");
-require_once(__DIR__ . '/vendor/autoload.php');
+
+require_once(__DIR__ . '/../vendor/autoload.php');
