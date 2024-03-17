253c1036b2cd6df6104e779cd911fd75b853a29d
DavidVilanova11 <david.vilanova@cirvianum.cat>
2024-03-17 20:42:43 +0100

    Refactor host and adn card rendering in fusio.view.php

--- App/Views/recuperada/fusio.view.php
+++ App/Views/recuperada/fusio.view.php
@@ -24,35 +24,52 @@ require_once(__DIR__ . "/../../Core/Store.php");
 <h1>SELECCIONA UN HOST</h1>
 
 <div class="container mt-5">
+    <!-- Hosts -->
     <div class="d-flex flex-wrap">
         <?php foreach ($params['llista'] as $tipo => $stocks) : ?>
-            <?php if (!empty($stocks)) : ?>
+            <?php if ($tipo === 'host' && !empty($stocks)) : ?>
                 <?php foreach ($stocks as $stock) : ?>
-                    <?php //echo '<pre>';
-                    // var_dump($tipo);
-                    // echo '</pre>';
-                    ini_set('display_errors', 1);
-                    ini_set('display_startup_errors', 1);
-                    error_reporting(E_ALL); ?>
-
-                    <?php if ($stock !== null && ((isset($stock['nom']) && $tipo === 'adn') || (isset($stock['especie']) && $tipo === 'host'))) : ?>
-                        <div class="card mx-3 my-3 <?php echo ($tipo === 'host' && isset($selectedHostId) && $selectedHostId === $stock['id']) ? 'selected' : ''; ?>" style="width: 16rem; height: 28rem; border: 2px solid transparent;" data-tipo="<?php echo $tipo; ?>" data-id="<?php echo $stock['id']; ?>">
-                            <div class="img-container">
-                                <img class="custom-image" src="../../../Public/img/<?php echo $tipo === 'adn' ? 'adn' : 'hosts'; ?>/<?php echo $stock['img']; ?>" alt="...">
-                            </div>
-                            <div class="card-body">
-                                <p class="card-text"><?php echo trim($tipo === 'adn' ? $stock['nom'] : $stock['especie']); ?></p>
-                                <?php if (isset($stock['quantity'])) : ?>
-                                    <p class="card-text"><?php echo "X" . $stock['quantity']; ?></p>
-                                <?php endif; ?>
-                            </div>
+                    <div class="card mx-3 my-3 <?php echo ($selectedHostId && $selectedHostId === $stock['id']) ? 'selected' : ''; ?>" style="width: 16rem; height: 28rem; border: 2px solid transparent;" data-tipo="<?php echo $tipo; ?>" data-id="<?php echo $stock['id']; ?>">
+                        <div class="img-container">
+                            <img class="custom-image" src="../../../Public/img/hosts/<?php echo $stock['img']; ?>" alt="...">
                         </div>
-                    <?php endif; ?>
+                        <div class="card-body">
+                            <p class="card-text"><?php echo trim($stock['especie']); ?></p>
+                            <?php if (isset($stock['quantity'])) : ?>
+                                <p class="card-text"><?php echo "X" . $stock['quantity']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </div>
+    <hr> <!-- Línea horizontal para separar los hosts de los adn -->
+
+    <!-- Adn -->
+    <div class="d-flex flex-wrap">
+        <?php foreach ($params['llista'] as $tipo => $stocks) : ?>
+            <?php if ($tipo === 'adn' && !empty($stocks)) : ?>
+                <?php foreach ($stocks as $stock) : ?>
+                    <div class="card mx-3 my-3" style="width: 16rem; height: 28rem; border: 2px solid transparent;" data-tipo="<?php echo $tipo; ?>" data-id="<?php echo $stock['id']; ?>">
+                        <div class="img-container">
+                            <img class="custom-image" src="../../../Public/img/adn/<?php echo $stock['img']; ?>" alt="...">
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text"><?php echo trim($stock['nom']); ?></p>
+                            <?php if (isset($stock['quantity'])) : ?>
+                                <p class="card-text"><?php echo "X" . $stock['quantity']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
                 <?php endforeach; ?>
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
 </div>
+
+
+
 
 <button id="confirmHostBtn" style="display: none;">Confirmar host</button>
 <button id="fusionBtn" style="display: none;">Fusionar</button>
